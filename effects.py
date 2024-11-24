5c98b20f5185cd2e9a147ea47733fdd8fb849f4e
EchoNoName <gu.ethan88@gmail.com>
2024-11-24 04:13:29 -0500

    added more effects used by cards and relics

--- effects.py
+++ effects.py
@@ -1,11 +1,16 @@
 import math
+import random
 
-def deal_damage(damage, targets, *args):
+def deal_damage(damage, targets, buffs, debuffs, *args):
+    actualDamage = damage + buffs['Strength'] + buffs['Vigour']
+    actualDamage = (actualDamage - debuffs['-Strength'])
+    if debuffs['Weak'] > 0:
+        actualDamage = actualDamage // 4
     if isinstance(targets, list):
         for entity in targets:
-            entity.damage_taken(damage)
+            entity.damage_taken(actualDamage)
     else:
-        targets.damage_taken(damage)
+        targets.damage_taken(actualDamage)
 
 def block_gain(block, targets, *args):
     if isinstance(targets, list):
@@ -13,6 +18,24 @@ def block_gain(block, targets, *args):
             entity.gain_block(block)
     else:
         targets.gain_block(block)
+
+def apply_buff(buffs, amount, targets):
+    if isinstance(targets, list):
+        for entity in targets:
+            for i in range(len(buffs)):
+                entity.gain_buffs(buffs[i], amount[i])
+    else:
+        for i in range(len(buffs)):
+            entity.gain_buffs(buffs[i], amount[i])
+
+def apply_debuff(debuffs, amount, targets):
+    if isinstance(targets, list):
+        for entity in targets:
+            for i in range(len(debuffs)):
+                entity.gain_debuffs(debuffs[i], amount[i])
+    else:
+        for i in range(len(debuffs)):
+            entity.gain_debuffs(debuffs[i], amount[i])
 
 def small_damage_reduction(damage, cap, *args): # The reduce small damage to 1 effect
     if damage <= cap and damage > 1:
@@ -27,4 +50,4 @@ def revive(revive_percentage, max_hp, *args):
 def healing_reduction(amount, reduction, *args):
     return max(amount - reduction, 0)
 
-#def discover(card_type, pile):
+# def discover(card_type, pile):
